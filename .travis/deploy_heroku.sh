69a24e0d5532585d711b7ca1a4f7f25fa008b9c7
Oseme Odigie <osemeodigie@yahoo.com>
2019-06-15 19:03:04 +0100

    feat: added the heroku deploy script

---
+++ .travis/deploy_heroku.sh
@@ -0,0 +1,7 @@
+#!/usr/env/sh
+
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+heroku plugins:install @heroku-cli/plugin-container-registry
+docker login –username –password=$HEROKU_API_KEY registry.heroku.com
+heroku container:push web –app $HEROKU_APP_NAME
+heroku container:release web –app $HEROKU_APP_NAME
